8c20035a3e06adf41e16b366d9d1545ff9634d0f
Tiago Terra <tiago@tiago-macbook.local>
2023-05-26 16:05:44 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Static QR Code Poem Generator
+
+A simple static website that leverages vanillaJS and the QRCodeJS library to generate a QR Code that will link to a poem
---
+++ script.js
@@ -0,0 +1,40 @@
+
+function submitForm(event) {
+  event.preventDefault();
+  
+  var poemText = document.getElementById('poem_text').value;
+  var poemAuthor = document.getElementById('poem_author').value;
+
+  generateQrCode(poemText, poemAuthor)
+
+}
+
+
+function generateQrCode(poemText, poemAuthor){
+  var baseUrl = window.location.hostname
+  divId = document.getElementById("qrcode")
+  divId.innerHTML = "" // resets div content before new generation
+  var urlWithParams = `${baseUrl}:5500/2023/new/poem-qr-generator/index.html?poem=${poemText}&author=${poemAuthor}`
+
+  new QRCode(divId, { text: urlWithParams,  width: 128, height: 128});
+  console.log(urlWithParams)
+}
+
+function displayPoem(){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const poem = urlParams.get('poem')
+  const author = urlParams.get('author')
+  console.log(author)
+
+  document.getElementById("poem_text").innerText = poem;
+  document.getElementById("poem_author").innerText = `- ${author}`;
+}
+
+window.onload = function(){
+
+  if (document.getElementById("poem") != null){
+    displayPoem()
+  }
+
+};
